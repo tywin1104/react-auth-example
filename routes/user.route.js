2d5aa97e51a9199796e247222b537480dd02f1ac
Alen Niu <alen.niu@FVHYK5MEJ1WK.local>
2019-08-23 20:57:58 -0400

    modify user level updates

--- routes/user.route.js
+++ routes/user.route.js
@@ -8,56 +8,63 @@ module.exports = router;
 router.get('/', function(req, res) {
     console.log(req.query)
     User.find(req.query, function(err, users) {
-      if(err) {
-        console.log(err)
-        res.status(500).json({
-          msg: "Unable to get all users"
-        })
-      }else {
-        res.status(200).json({users})
-      }
+        if (err) {
+            console.log(err)
+            res.status(500).json({
+                msg: "Unable to get all users"
+            })
+        } else {
+            res.status(200).json({ users })
+        }
     })
-  })
+})
 
 //Get User by Id
 router.get('/:user_id', function(req, res) {
     User.findById(req.params.user_id, function(err, user) {
-      if(err) {
-        console.log(err)
-        res.status(500).json({
-          msg: "Unable to get user by id"
-        })
-      }else {
-        res.status(200).json({user})
-      }
+        if (err) {
+            console.log(err)
+            res.status(500).json({
+                msg: "Unable to get user by id"
+            })
+        } else {
+            res.status(200).json({ user })
+        }
     })
-  })
+})
 
-  //Partial update
+//Partial update
 router.patch('/:user_id', function(req, res) {
-    User.findByIdAndUpdate(req.params.user_id, req.body, {new: true}, function(err, user) {
-      if(err) {
-        console.log(err)
-        res.status(500).json({
-          msg: "Unable to update user"
-        })
-      }else {
-        res.status(200).json({user})
-      }
-    })
-  })
-
-  //Add group info to user
-router.post('/:user_id/groups', function(req, res) {
-  const {group} = req.body
-  User.findByIdAndUpdate(req.params.user_id, {$push: {groups: group}}, {new: true}, function (err, user) {
-    if (err) {
-      console.log(err);
-      res.status(500).json({
-        msg: "unable to add group info to this user"
-      })
-    } else {
-      res.status(200).json(user);
+    let points = req.body.points;
+    update = req.body;
+    if (points) {
+        let level = 1 + parseInt(points / 10);
+        update.level = level;
     }
-  });
+    console.log(update)
+    User.findByIdAndUpdate(req.params.user_id, update, { new: true }, function(err, user) {
+        if (err) {
+            console.log(err)
+            res.status(500).json({
+                msg: "Unable to update user"
+            })
+        } else {
+            res.status(200).json({ user })
+        }
+    })
+})
+
+//Add group info to user
+router.post('/:user_id/groups', function(req, res) {
+    const { group } = req.body
+    User.findByIdAndUpdate(req.params.user_id, { $push: { groups: group } }, { new: true }, function(err, user) {
+        if (err) {
+            console.log(err);
+            res.status(500).json({
+                msg: "unable to add group info to this user"
+            })
+        } else {
+            res.status(200).json(user);
+        }
+    });
 })
