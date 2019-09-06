28509b03d902cb6cf98eeb7c2bf75fb9c57a4a5b
Alen Niu <alen.niu@FVHYK5MEJ1WK.lan>
2019-09-05 20:49:02 -0400

    render html

--- server.js
+++ server.js
@@ -7,12 +7,17 @@ var cors = require('cors')
 const app = express();
 
 
+app.set('view engine', 'html')
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cookieParser());
+app.use(express.static(__dirname))
 
 app.use(cors())
 app.use('/api/', routes);
+app.get('/', (req, res) => {
+  res.render('index')
+})
 
 const mongo_uri = "mongodb://alen:kootracats@mentr-shard-00-00-2httx.mongodb.net:27017,mentr-shard-00-01-2httx.mongodb.net:27017,mentr-shard-00-02-2httx.mongodb.net:27017/mentr?ssl=true&replicaSet=mentr-shard-0&authSource=admin&retryWrites=true&w=majority";
 
