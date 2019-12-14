77acc22f71c1c7b19a3e2dc6ce12cefa1db90a78
root <root@ip-172-31-41-202.ec2.internal>
2019-12-14 17:30:23 +0000

    test remove

--- models/Announcement.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-
-const AnnouncementSchema = new mongoose.Schema({
-    content: {type: String, required:true},
-    timestamp: {type: Date, default: Date.now, required:true}
-});
-
-module.exports = mongoose.model('Announcement', AnnouncementSchema);
