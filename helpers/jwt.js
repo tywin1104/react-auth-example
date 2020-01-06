c8702ef00e666f4969447d57ce8e9a7dbe75b1e6
root <root@ip-172-31-41-202.ec2.internal>
2020-01-06 13:02:27 +0000

    dww

--- helpers/jwt.js
+++
@@ -1,9 +0,0 @@
-const jwt = require('jsonwebtoken')
-const secret = 'qiehquiorhutuqhruijq3r'
-
-module.exports.encode = payload => jwt.sign(payload, secret)
-module.exports.decode = token => {
-    let decoded = jwt.decode(token, secret)
-    console.log('decoded : ', decoded)
-    return decoded
-}
