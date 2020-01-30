680e6ebe1f1d43230c3ef49da17c232c86f7b66c
root <root@ip-172-31-41-202.ec2.internal>
2020-01-30 17:23:26 +0000

    t

--- models/Announcement.js
+++ models/Announcement.js
@@ -4,7 +4,8 @@ const mongoose = require('mongoose');
 const AnnouncementSchema = new mongoose.Schema({
     content: {type: String, required:true},
     timestamp: {type: Date, default: Date.now, required:true},
-    mini: {type: String}
+    mini: {type: String},
+    pic: {type: String}
 });
 
 module.exports = mongoose.model('Announcement', AnnouncementSchema);
