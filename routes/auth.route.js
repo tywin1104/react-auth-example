9bb74840af123e6041bfa0a789f34ded82045920
root <root@ip-172-31-41-202.ec2.internal>
2020-04-01 18:47:23 +0000

    dd

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -9,66 +9,70 @@ router.post('/register', function(req, res) {
     const { email, password, name } = req.body;
     const user = new User({ email, password, name });
     user.save(function(err) {
-      if (err) {
-        console.log(err);
-        res.status(500).send("Error registering new user please try again.");
-      } else {
-        res.status(200).send("Welcome to the club!");
-      }
+        if (err) {
+            console.log(err);
+            res.status(500).send("Error registering new user please try again.");
+        } else {
+            res.status(200).send("Welcome to the club!");
+        }
     });
 });
 
 
-router.get("/test", function(req, res){
-  res.send("work")  
+router.get("/", (req, res) => {
+    console.log("HMM")
+})
+
+router.get("/test", function(req, res) {
+    res.send("work")
 })
 
 router.post('/authenticate', function(req, res) {
-  console.log('login')
+    console.log('login')
     const { email, password } = req.body;
     User.findOne({ email }, function(err, user) {
-      if (err) {
-        console.error(err);
-        res.status(500)
-          .json({
-          error: 'Internal error please try again'
-        });
-      } else if (!user) {
-        res.status(401)
-          .json({
-          error: 'Incorrect email or password'
-        });
-      } else {
-        user.isCorrectPassword(password, function(err, same) {
-          if (err) {
+        if (err) {
+            console.error(err);
             res.status(500)
-              .json({
-              error: 'Internal error please try again'
-            });
-          } else if (!same) {
+                .json({
+                    error: 'Internal error please try again'
+                });
+        } else if (!user) {
             res.status(401)
-              .json({
-              error: 'Incorrect email or password'
+                .json({
+                    error: 'Incorrect email or password'
+                });
+        } else {
+            user.isCorrectPassword(password, function(err, same) {
+                if (err) {
+                    res.status(500)
+                        .json({
+                            error: 'Internal error please try again'
+                        });
+                } else if (!same) {
+                    res.status(401)
+                        .json({
+                            error: 'Incorrect email or password'
+                        });
+                } else {
+                    // Issue token
+                    const payload = {
+                        _id: user._id,
+                        email: email,
+                        name: user.name,
+                        role: user.userType,
+                        groups: user.groups
+                    };
+                    res.setHeader('token', encode(payload))
+                    res.status(200).send({ token: encode(payload), id: user._id });
+                }
             });
-          } else {
-            // Issue token
-            const payload = {
-              _id: user._id,
-              email: email,
-              name: user.name,
-              role:user.userType,
-              groups:user.groups
-            };
-            res.setHeader('token', encode(payload))
-            res.status(200).send({token:encode(payload)});
-          }
-        });
-      }
+        }
     });
 });
 
 
 router.get('/checkToken', authenticate, function(req, res) {
     console.log(`${req.user.email} | ${req.user.username} checked in`)
-    res.status(200).json({email: req.user.email, name: req.user.username});
+    res.status(200).json({ email: req.user.email, name: req.user.username });
 });
